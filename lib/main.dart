58bd869c75fb9cb026ac9edc9dbf5df39324454c
‘terupro’ <terupro911@gmail.com>
2022-05-08 08:48:07 +0900

    udate

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Clima',
       theme: ThemeData(
         appBarTheme: kAppBarTheme,
         scaffoldBackgroundColor: kScaffoldBackgroundColor,
